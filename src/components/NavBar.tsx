b0e64f8c36e15a75bb6b29851a6ea24e29d90973
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2023-04-26 23:25:48 -0500

    adding some items for the navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,18 @@
+import {close,logo,menu} from '../assets';
+import {navLinks} from '../constants'
+const NavBar = () => {
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="bitcoins" className="w-[124px] h-[32px]" />
+
+      <ul className="list-no sm:flex hidden justify-end items-center flex-1 ">
+        {navLinks.map((nav,index)=>(
+          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index===navLinks.length -1 ? 'mr-0':'mr-10'}`}><a href={`#${nav.id}`}/>{nav.title}</li>
+        ))}
+
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar
