ef45ce5d87b163b6350abd6c507b9a5faec9d241
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2023-05-05 23:06:58 -0500

    home and getting stated disign ok

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,30 @@
+import { useState } from 'react';
 import {close,logo,menu} from '../assets';
 import {navLinks} from '../constants'
 const NavBar = () => {
+  const [toggle, settoggle] = useState(false)
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="bitcoins" className="w-[124px] h-[32px]" />
 
-      <ul className="list-no sm:flex hidden justify-end items-center flex-1 ">
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1 ">
         {navLinks.map((nav,index)=>(
           <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index===navLinks.length -1 ? 'mr-0':'mr-10'}`}><a href={`#${nav.id}`}/>{nav.title}</li>
         ))}
 
       </ul>
+
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img src={toggle?close:menu} alt="menu" className="w-[28px] h-[28px] object-contain" onClick={()=>settoggle((prev)=>!prev)} />
+
+        <div className={`${toggle?'flex':'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+        <ul className="list-none flex flex-col justify-end items-center flex-1 ">
+        {navLinks.map((nav,index)=>(
+          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index===navLinks.length -1 ? 'mr-0':'mb-4'}`}><a href={`#${nav.id}`}/>{nav.title}</li>
+        ))}
+      </ul>
+        </div>
+      </div>
     </nav>
   )
 }
